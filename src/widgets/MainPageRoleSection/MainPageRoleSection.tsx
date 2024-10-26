28e1c65438d6089b0d97ae1b242d41da6a48ed42
Vladislav-Zakrevskiy <zakrevskiyvlada@yandex.ru>
2024-10-26 04:34:46 +0300

    added antd

--- src/widgets/MainPageRoleSection/MainPageRoleSection.tsx
+++ src/widgets/MainPageRoleSection/MainPageRoleSection.tsx
@@ -1,9 +1,11 @@
-import { useUserStore } from "@/entities/User";
+import { UserRoles, useUserStore } from "@/entities/User";
 import { getRole } from "@/shared/lib/utils/getRole";
 
 export const MainPageRoleSection = () => {
 	const { userInfo } = useUserStore();
 
-	switch (getRole(userInfo?.roles)) {
+	switch (getRole(userInfo?.roles || [])) {
+		case UserRoles.ADMIN:
+			return;
 	}
 };
